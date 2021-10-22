9e503be4959599b75f74f0f6c882e8deace8edd9
Nathaniel Betancourt <nathanielbetancourt@hotmail.com>
2021-10-22 11:49:58 -0400

    add database connect

---
+++ includes/config.php
@@ -0,0 +1,17 @@
+<?php
+
+ob_start(); //output buffering
+session_start();
+
+date_default_timezone_set("America/New_York")
+
+try {
+        $con = new PDO ("mysql:dbname=fflix;host=localhost", "root", "");
+        $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+}
+catch (PDOException $e) {
+
+
+}
+
+?>
