822baa82a8f62b45d9a0000859b6d711d8fc2960
Nathaniel Betancourt <nathanielbetancourt@hotmail.com>
2021-10-22 11:57:22 -0400

    add account class

--- includes/config.php
+++ includes/config.php
@@ -3,14 +3,15 @@
 ob_start(); //output buffering
 session_start();
 
-date_default_timezone_set("America/New_York")
+date_default_timezone_set("America/New_York");
 
 try {
         $con = new PDO ("mysql:dbname=fflix;host=localhost", "root", "");
         $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 }
-catch (PDOException $e) {
 
+catch (PDOException $e) {
+        exit("CONNECTION FAILED: " . $e->getMessage());
 
 }
 
